2f7315bdb44363799d45cd0c1dafd3d2ddbad00b
Suneelvijay M <136710677+Suneelvijay@users.noreply.github.com>
2023-12-13 22:57:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-import streamlit as st
+import matplotlib.pyplot as plt
 
-def main():
-    st.title("Electricity Tariff Calculator")
+# Define input variables
+frequency_change = ctrl.Antecedent(np.arange(-1, 1.1, 0.1), 'frequency_change')
+consumption = ctrl.Antecedent(np.arange(-1, 1.1, 0.1), 'consumption')
 
-    # Define input variables
-    frequency_change = ctrl.Antecedent(np.arange(-1, 1.1, 0.1), 'frequency_change')
-    consumption = ctrl.Antecedent(np.arange(-1, 1.1, 0.1), 'consumption')
+# Define output variable
+electricity_tariff = ctrl.Consequent(np.arange(-1, 1.1, 0.1), 'electricity_tariff')
 
-    # Define output variable
-    electricity_tariff = ctrl.Consequent(np.arange(-1, 1.1, 0.1), 'electricity_tariff')
+# Define linguistic variables for frequency_change and consumption
+frequency_change['N'] = fuzz.trimf(frequency_change.universe, [-2, -1, 0])
+frequency_change['Z'] = fuzz.trimf(frequency_change.universe, [-1, 0, 1])
+frequency_change['P'] = fuzz.trimf(frequency_change.universe, [0, 1, 2])
+consumption['N'] = fuzz.trimf(consumption.universe, [-2, -1, 0])
+consumption['Z'] = fuzz.trimf(consumption.universe, [-1, 0, 1])
+consumption['P'] = fuzz.trimf(consumption.universe, [0, 1, 2])
 
-    # Define linguistic variables and rules (same as before)
+# Define linguistic variables for electricity_tariff
+electricity_tariff['L'] = fuzz.trimf(electricity_tariff.universe, [-2, -1, 0])
+electricity_tariff['M'] = fuzz.trimf(electricity_tariff.universe, [-1, 0, 1])
+electricity_tariff['H'] = fuzz.trimf(electricity_tariff.universe, [0, 1, 2])
 
-    # Create control system and simulation
-    electricity_tariff_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
-    electricity_tariff_simulation = ctrl.ControlSystemSimulation(electricity_tariff_ctrl)
+# Define rules
+rule1 = ctrl.Rule(antecedent=(frequency_change['P'] & consumption['P']), consequent=electricity_tariff['L'])
+rule2 = ctrl.Rule(antecedent=(frequency_change['Z'] & consumption['P']), consequent=electricity_tariff['H'])
+rule3 = ctrl.Rule(antecedent=(frequency_change['N'] & consumption['P']), consequent=electricity_tariff['H'])
+rule4 = ctrl.Rule(antecedent=(frequency_change['P'] & consumption['Z']), consequent=electricity_tariff['M'])
+rule5 = ctrl.Rule(antecedent=(frequency_change['Z'] & consumption['Z']), consequent=electricity_tariff['M'])
+rule6 = ctrl.Rule(antecedent=(frequency_change['N'] & consumption['Z']), consequent=electricity_tariff['M'])
+rule7 = ctrl.Rule(antecedent=(frequency_change['P'] & consumption['N']), consequent=electricity_tariff['M'])
+rule8 = ctrl.Rule(antecedent=(frequency_change['Z'] & consumption['N']), consequent=electricity_tariff['M'])
+rule9 = ctrl.Rule(antecedent=(frequency_change['N'] & consumption['N']), consequent=electricity_tariff['L'])
 
-    # Streamlit sidebar input fields
-    with st.sidebar:
-        st.subheader("Enter Input Values")
-        frequency = st.slider("Frequency Change", -1.0, 1.0, step=0.1)
-        consumed = st.slider("Consumption rate", -1.0, 1.0, step=0.1)
-        calculate = st.button("Calculate")
+# Create and simulate the control system
+electricity_tariff_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
+electricity_tariff_simulation = ctrl.ControlSystemSimulation(electricity_tariff_ctrl)
 
-    # Calculate and display the result
-    if calculate:
-        electricity_tariff_simulation.input['frequency_change'] = frequency
-        electricity_tariff_simulation.input['consumption'] = consumed
-        electricity_tariff_simulation.compute()
+# Pass inputs to the simulation
+frequency = float(input("Enter Frequncy Change: "))  # Example input values
+consumed = float(input("Enter Consumption rate: "))  # Example input values
 
-        tariff_output = electricity_tariff_simulation.output['electricity_tariff']
-        base = 4.5
-        change = base * tariff_output
-        final = base + change
+electricity_tariff_simulation.input['frequency_change'] = frequency
+electricity_tariff_simulation.input['consumption'] = consumed
 
-        st.subheader("Result")
-        st.write(f"Tariff Change: {tariff_output*100: .2f} %")
-        st.write(f"Actual Base Charge: ₹{base}/-")
-        st.write(f"Updated Charge Per-Unit: ₹{final:.2f}/-")
+# Compute the result
+electricity_tariff_simulation.compute()
 
-if __name__ == "__main__":
-    main()
+# Get the computed output
+
+tariff_output = electricity_tariff_simulation.output['electricity_tariff']
+print(f"Tariff Change: {tariff_output*100: .2f} %")
+
+# Electricity Tariff Updation
+base = 4.5
+change = base * tariff_output
+final = base + change
+print(f"Actual Base Charge: ₹{base}/-")
+print(f"Updated Charge Per-Unit: ₹{final:.2f}/-")
